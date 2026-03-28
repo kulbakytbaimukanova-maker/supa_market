4d0d618b77c82198045cc3c0ec8db77aba2afd2d
kulbakytbaimukanova-maker <kulbakytbaimukanova@gmail.com>
2026-03-28 23:38:44 +0500

    Restore imports and OAuth registration in app.py

--- test/app.py
+++ test/app.py
@@ -52,7 +52,7 @@ oauth.register(
     name='google',
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    redirect_uri = "https://supa-market.onrender.com/auth/callback" ,
     client_kwargs={'scope': 'openid email profile'}
 )
 
