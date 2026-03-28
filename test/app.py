f4a995360246f92b7aaec6ad95f94166fb11380d
kulbakytbaimukanova-maker <kulbakytbaimukanova@gmail.com>
2026-03-28 23:41:02 +0500

    Refactor app.py by re-adding imports and functions

--- test/app.py
+++ test/app.py
@@ -52,7 +52,7 @@ oauth.register(
     name='google',
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
-    redirect_uri = "https://supa-market.onrender.com/auth/callback" ,
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
 
